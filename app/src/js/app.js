edaee97bb93e0507593bba8f26b1b47f55d0074f
aaron <alkrauss48@gmail.com>
2017-05-15 12:52:23 -0500

    Add countdown library and logic

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,8 +1,10 @@
 import skipMenuHandler from "./lib/skipMenuHandler.js";
 import isMobile from "./lib/isMobile.js";
+import {countdownHandler} from "./lib/countdownHandler.js";
 
 $(document).ready(function(){
   skipMenuHandler.init();
+  countdownHandler.init();
 
   // Remove 'focus' styles when you click a link
   $('a, button').mouseup(function() { this.blur() });

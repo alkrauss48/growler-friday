dc0f9dc4a5f471ffa71d18530e92f50f9f102392
aaron <alkrauss48@gmail.com>
2017-05-15 20:12:35 -0500

    Get countdown working

--- app/src/js/lib/countdownHandler.js
+++ app/src/js/lib/countdownHandler.js
@@ -2,21 +2,36 @@ export var countdownHandler = {
   init
 };
 
+const countdownTimes = [{
+  value: '%-d',
+  caption: 'Day%!d'
+},{
+  value: '%H',
+  caption: 'Hours'
+},{
+  value: '%S',
+  caption: 'Minutes'
+},{
+  value: '%S',
+  caption: 'Seconds'
+}];
+
 function init() {
   $('#clock').countdown('2020/10/10 12:34:56')
     .on('update.countdown', function(event) {
-      var format = '%H:%M:%S';
-      if(event.offset.totalDays > 0) {
-        format = '%-d day%!d ' + format;
-      }
-      if(event.offset.weeks > 0) {
-        format = '%-w week%!w ' + format;
-      }
+      let format = ''
+      countdownTimes.forEach( (timeUnit) => {
+        format += `
+          <figure class="countdown__item">
+            <h3 class="countdown__unit">${timeUnit.value}</h3>
+            <figcaption class="countdown__caption">${timeUnit.caption}</figcaption>
+          </figure>
+        `;
+      });
       $(this).html(event.strftime(format));
     })
     .on('finish.countdown', function(event) {
       $(this).html('This offer has expired!')
         .parent().addClass('disabled');
-
     });
 }
